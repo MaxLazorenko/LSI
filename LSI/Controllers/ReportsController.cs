3b8844f85effadcba4762d0e7253ff2d1dc98178
Max <mr.syrex@gmail.com>
2021-10-28 18:25:41 +0200

    fix: Use IRequest model instead of parameters

--- LSI/Controllers/ReportsController.cs
+++ LSI/Controllers/ReportsController.cs
@@ -16,17 +16,9 @@ namespace LSI.Controllers
         }
 
         [HttpGet]
-        public Task<IActionResult> GetReports([FromQuery] string roomName, 
-                                              [FromQuery] string from, 
-                                              [FromQuery] string to, 
+        public Task<IActionResult> GetReports([FromQuery] GetReportListQuery query,  
                                               CancellationToken token)
         {
-            var query = new GetReportListQuery
-            {
-                RoomName = roomName,
-                From = from,
-                To = to
-            };
             return FromQueryResult(query, token);
         }
     }
